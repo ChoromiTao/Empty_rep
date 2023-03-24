3b9f41b73c6eb7b1535c50ca96a9937d9aa29af9
Choromi Tao <princessailish94@mail.ru>
2023-03-25 00:47:23 +0900

    Create new programm

---
+++ Program/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+string[] GetArray()
+{
+    Console.WriteLine("Input number of length array");
+    int a = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[a];
+    Console.WriteLine("And now fill array: ");
+    for (int i = 0; i < a; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
