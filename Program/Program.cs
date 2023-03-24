7c89db1d0e1779bebaefbb8648242e63f987ee1d
Choromi Tao <princessailish94@mail.ru>
2023-03-25 00:47:59 +0900

    Done new programm

--- Program/Program.cs
+++ Program/Program.cs
@@ -12,3 +12,21 @@ string[] GetArray()
     }
     return array;
 }
+
+string[] SortArray(string[] array)
+{
+    string[] sortArray = new string[array.Length];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <=3)
+        {
+            sortArray[j] = array[i];
+            Console.Write(sortArray[j] + ",");
+            j++;
+        }
+    }
+    return sortArray;
+}
+
+string[] array1 = GetArray();
+string[] array2 = SortArray(array1);
